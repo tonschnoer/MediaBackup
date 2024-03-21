46ac0bc1af33105bdb2b7cc1dc0624469dae1012
tonschnoer <kmtegtmeier@tonschnoer.de>
2024-03-21 12:53:26 +0100

    new branch

--- MediaBackup2/src/main/java/de/kmt/MediaBackup/TCPClient.java
+++ MediaBackup2/src/main/java/de/kmt/MediaBackup/TCPClient.java
@@ -12,53 +12,60 @@ import org.apache.logging.log4j.Logger;
 
 public class TCPClient {
 	protected final Logger logger = LogManager.getLogger();
-	
+
 	public void main(String[] args) {
 		byte[] buffer = new byte[32768];
 
 		try {
-			
+
 			logger.info("Startup TCPClient");
-			DataOutputStream dos = new DataOutputStream(new FileOutputStream(Config.getConfigKey("directory") + Config.getConfigKey("file")));
-			Socket clientSocket = new Socket(Config.getConfigKey("server"),12345);
+			DataOutputStream dos = new DataOutputStream(
+					new FileOutputStream(Config.getConfigKey("directory") + Config.getConfigKey("file")));
+			Socket clientSocket = new Socket(Config.getConfigKey("server"), 12345);
 			OutputStreamWriter out = new OutputStreamWriter(clientSocket.getOutputStream());
 			InputStream is = clientSocket.getInputStream();
-			
-			
+
 			while (true) {
-					
+
 				// request file from server
 				out.write(Config.getConfigKey("file"));
 				out.flush();
 
 				while (true) {
-					
+
 					// read up to 32768 bytes from stream
-					int readbytes = is.read(buffer,0,32768);
+					int readbytes = is.read(buffer, 0, 32768);
 					System.out.print(".");
 
 					// check if 32768 bytes where read (i.e. there are some more)
-					if (readbytes<32768) {
-						
-						// less than 32768 bytes are read. Check if last 3 bytes are "END". If so: stop receiving data
-						if ((buffer[readbytes-3]==0x45) & (buffer[readbytes-2]==0x4e) & (buffer[readbytes-1]==0x44)) {
-							System.out.println("Schluss");
-							
-							// if there is some interesting data, before END, write it to file
-							if (readbytes>3) {
-								dos.write(buffer,0,readbytes-3);
+					if (readbytes < 32768) {
+
+						if (readbytes > 2) {
+							// less than 32768 bytes are read. Check if last 3 bytes are "END". If so: stop
+							// receiving data
+							if ((buffer[readbytes - 3] == 0x45) & (buffer[readbytes - 2] == 0x4e)
+									& (buffer[readbytes - 1] == 0x44)) {
+								System.out.println("Schluss");
+
+								// if there is some interesting data, before END, write it to file
+								if (readbytes > 3) {
+									dos.write(buffer, 0, readbytes - 3);
+								}
+								break;
+							} else {
+								// if bytesread < 32768, but no ending Tag "END" -> write buffer to file
+								dos.write(buffer, 0, readbytes);
 							}
-							break;
 						} else {
-							// if bytesread < 32768, but no ending Tag "END" -> write buffer to file
-							dos.write(buffer,0,readbytes);
+							dos.write(buffer, 0, readbytes);
 						}
+
 					} else {
 						// if bytesread = 32768, assume there is more and write buffer to file
-						dos.write(buffer,0,readbytes);
+						dos.write(buffer, 0, readbytes);
 					}
 				}
-				
+
 				// close output file
 				dos.close();
 
@@ -67,7 +74,7 @@ public class TCPClient {
 
 				// Close the socket and streams for this client
 				clientSocket.close();
-				
+
 				// exit while(true)
 				break;
 			}
@@ -76,6 +83,6 @@ public class TCPClient {
 			System.out.println("An exception has occurred:");
 			e.printStackTrace();
 		}
-	
+
 	}
 }
